92c39a665c5333233d7c0fd8131461627ae2888c
Karolina <k4aro0@gmail.com>
2024-05-30 07:08:31 +0200

    add database connection

---
+++ App/DAL/DBConnection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace App.DAL
+{
+    class DBConnection
+    {
+        private MySqlConnectionStringBuilder stringBuilder = new MySqlConnectionStringBuilder();
+        private static DBConnection instance = null;
+        private static DBConnection Instance
+        {
+            get
+            {
+                if (instance == null) instance = new DBConnection();
+                return instance;
+            }
+        }
+
+        private MySqlConnection connection;
+        public MySqlConnection Connection => connection;
+
+        private DBConnection()
+        {
+            stringBuilder.UserID = Properties.Settings.Default.user;
+            stringBuilder.Server = Properties.Settings.Default.server;
+            stringBuilder.Database = Properties.Settings.Default.database;
+            stringBuilder.Port = Properties.Settings.Default.port;
+            stringBuilder.Password = Properties.Settings.Default.password;
+
+            connection = new MySqlConnection(stringBuilder.ToString());
+        }
+    }
+}
