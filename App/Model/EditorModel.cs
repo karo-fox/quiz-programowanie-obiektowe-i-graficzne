d3a2c8d3079df10f5b9e4866878fa055bda38e10
kicerKs <osk.ch18@gmail.com>
2024-05-31 23:30:25 +0200

    Editor startup

---
+++ App/Model/EditorModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Model
+{
+    using DAL.Entities;
+    using DAL.Repositories;
+    using System.Collections.ObjectModel;
+
+    internal class EditorModel
+    {
+        public ObservableCollection<int> QuestionIds;
+        public EditorModel() 
+        { 
+            
+        }
+    }
+}
